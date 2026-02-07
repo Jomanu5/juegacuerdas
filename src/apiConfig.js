1629d8a0b1a33b1ca7843342d32a76ea4f017882
José Manuel <josemanuelharri@gmail.com>
2026-02-07 17:50:43 -0300

    borrado localhost

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
-
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export const API_TIENDA = `${BASE_URL}/api-tienda`
 export const API_ESCUELA = `${BASE_URL}/api-escuela`
